a0c06da2e37df77f5474a0b4aadb6a4c293e432a
mauriciohc02 <mauriciohdzc02@gmail.com>
2023-04-08 05:52:38 +0000

    Modificando las credenciales

--- controller/connection.php
+++ controller/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 	$dsn = 'mysql:dbname=qpet;host=localhost';
-	$user = 'root';
-	$password = 'Daniel_12';
+	$user = 'admin';
+	$password = '509a10d50e7ed65a4a1a0c6817a67fe07f985c973d275989';
 
 	try{
 		$pdo = new PDO($dsn, $user, $password);
