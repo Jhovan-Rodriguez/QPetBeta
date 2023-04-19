bac51a9f81beaab15de46a4718aa66eae67c0cc4
Daniel Eduardo Macias Estrada <danma1708@gmail.com>
2023-04-18 21:07:49 -0600

    Update connection.php

--- controller/connection.php
+++ controller/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 	$dsn = 'mysql:dbname=qpet;host=localhost';
-	$user = 'admin';
-	$password = '509a10d50e7ed65a4a1a0c6817a67fe07f985c973d275989';
+	$user = 'root';
+	$password = 'Daniel_12';
 
 	try{
 		$pdo = new PDO($dsn, $user, $password);
